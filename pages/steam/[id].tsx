7dff636ef2eea34f836b0baf5c7c72ec76277ac5
windo-developer <yui050505@gmail.com>
2022-08-24 01:24:32 -0700

    Ui-3

---
+++ pages/steam/[id].tsx
@@ -0,0 +1,87 @@
+import type { NextPage } from "next";
+
+const Steam: NextPage = () => {
+  return (
+    <div className="py-10 px-4 space-y-4">
+      <div className="w-full bg-slate-400 aspect-video rounded-md" />
+      <h3 className="mt-2 text-gray-700 text-2xl font-semibold">stream name</h3>
+      <div className="py-10 pb-16 h-[50vh] px-4 space-y-6 overflow-y-scroll">
+        <div className="flex items-start space-x-2">
+          <div className="w-8 h-8 rounded-full bg-slate-300" />
+          <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex items-start space-x-2">
+          <div className="w-8 h-8 rounded-full bg-slate-300" />
+          <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex items-start space-x-2">
+          <div className="w-8 h-8 rounded-full bg-slate-300" />
+          <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex items-start space-x-2">
+          <div className="w-8 h-8 rounded-full bg-slate-300" />
+          <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex items-start space-x-2">
+          <div className="w-8 h-8 rounded-full bg-slate-300" />
+          <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex items-start space-x-2">
+          <div className="w-8 h-8 rounded-full bg-slate-300" />
+          <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex items-start space-x-2">
+          <div className="w-8 h-8 rounded-full bg-slate-300" />
+          <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex items-start space-x-2">
+          <div className="w-8 h-8 rounded-full bg-slate-300" />
+          <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex items-start space-x-2">
+          <div className="w-8 h-8 rounded-full bg-slate-300" />
+          <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+        <div className="flex items-start space-x-2">
+          <div className="w-8 h-8 rounded-full bg-slate-300" />
+          <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
+        </div>
+      </div>
+      <div className="fixed w-full mx-auto max-w-md inset-x-0 bottom-2">
+        <div className="relative flex items-center">
+          <input
+            type="text"
+            className="pr-12 shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none focus:border-orange-500"
+          />
+          <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
+            <button className="flex items-center bg-orange-500 rounded-full px-3 text-sm text-white hover:bg-orange-600 focus:ring-2 focus:ring-offset-2 focus:ring-orange-500">
+              &rarr;
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Steam;
